de4ab86ea0d962dd188ced2a5bb0618cf14206e7
richmondb <richmond.billones@gmail.com>
2025-01-30 17:24:49 +0800

    added preference dark mode in config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  colorMode:{
+    preference: 'dark',
+    fallback: 'light',
+  },
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   modules: ["@nuxt/ui", "@nuxt/eslint"],
